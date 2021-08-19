1d426cf1c848ba13fe8801b04349822f88e743ef
mrabhi05 <nair.abhi0505@gmail.com>
2021-08-19 12:38:18 +0530

    Completed the Cart Page

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAI2M1Qj0f-2UlmGvd4saclw6a8IIzlAjg",
+    authDomain: "clone2-ff671.firebaseapp.com",
+    projectId: "clone2-ff671",
+    storageBucket: "clone2-ff671.appspot.com",
+    messagingSenderId: "809105486991",
+    appId: "1:809105486991:web:7ff19d4645a66f1e956cfa",
+    measurementId: "G-KT8BJMLZSS"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
